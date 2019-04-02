43319e09710a61941bec5b5a141e7f41635a644c
Huifeng Guan <hguan@google.com>
2019-04-02 14:12:38 -0700

    add company logos

--- generateEXCEL/ModifyHTML.py
+++ generateEXCEL/ModifyHTML.py
@@ -12,6 +12,7 @@ content = content.replace("AMD-Logo","<img src='Img/AMD.png' width=16 height=16>
 content = content.replace("Apple-Logo","<img src='Img/Apple.png' width=16 height=16>")
 content = content.replace("Grab-Logo","<img src='Img/Grab.png' width=16 height=16>")
 content = content.replace("PureStorage-Logo","<img src='Img/PureStorage.png' width=16 height=16>")
+content = content.replace("Walmart-Logo","<img src='Img/Walmart.jpeg' width=16 height=16>")
 
 
 
