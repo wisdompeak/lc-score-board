ac981b192bff4e2ac7f2f72dfa4a1f01469226bb
wisdompeak <guan.huifeng@gmail.com>
2019-06-29 22:55:38 -0700

    update curve

--- generateEXCEL/ModifyHTML.py
+++ generateEXCEL/ModifyHTML.py
@@ -36,6 +36,7 @@ content = content.replace("Citi-Logo","<img src='Img/Citi.jpeg' width=22 height=
 content = content.replace("Petuum-Logo","<img src='Img/Petuum.jpeg' width=18 height=18>")
 content = content.replace("Ebay-Logo","<img src='Img/Ebay.png' width=23 height=16>")
 content = content.replace("DXC-Logo","<img src='Img/DXC.png' width=20 height=16>")
+content = content.replace("Cruise-Logo","<img src='Img/Cruise.png' width=22 height=16>")
 
 
 content = content.replace("curve-figure","<img src='Img/curve.png'>")
