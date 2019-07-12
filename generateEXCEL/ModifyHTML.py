417e686b73450b5b210ce3a4c2010ae85c2ccd53
Huifeng Guan <hguan@google.com>
2019-07-12 15:11:26 -0700

    add Zhihu

--- generateEXCEL/ModifyHTML.py
+++ generateEXCEL/ModifyHTML.py
@@ -37,7 +37,7 @@ content = content.replace("Petuum-Logo","<img src='Img/Petuum.jpeg' width=18 hei
 content = content.replace("Ebay-Logo","<img src='Img/Ebay.png' width=23 height=16>")
 content = content.replace("DXC-Logo","<img src='Img/DXC.png' width=20 height=16>")
 content = content.replace("Cruise-Logo","<img src='Img/Cruise.png' width=22 height=16>")
-
+content = content.replace("Zhihu-Logo","<img src='Img/Zhihu.jpeg' width=18 height=18>")
 
 content = content.replace("curve-figure","<img src='Img/curve.png'>")
 
