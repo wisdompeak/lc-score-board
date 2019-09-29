8b7b4d92ffa142d7ccc65c23b5a24623812aff7c
wisdompeak <guan.huifeng@gmail.com>
2019-09-28 21:46:08 -0700

    contest 156

--- getRank/RecordData.py
+++ getRank/RecordData.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("contest", help="contest id")
 kwargs = parser.parse_args()
 contest = int(kwargs.contest)
-pageNum = 170
+pageNum = 200
 
 results = fetchRanking(contest,pageNum)
 
