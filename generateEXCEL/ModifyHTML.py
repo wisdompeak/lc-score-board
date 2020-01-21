4592d54044dbf89b159db976f592c6a46309fa06
Huifeng Guan <hguan@google.com>
2020-01-21 15:10:53 -0800

    update companies

--- generateEXCEL/ModifyHTML.py
+++ generateEXCEL/ModifyHTML.py
@@ -57,9 +57,11 @@ content = content.replace("TwoSigma-Logo","<img src='Img/TwoSigma.jpg' width=18 
 content = content.replace("ICANN-Logo","<img src='Img/ICANN.png' width=18 height=18>")
 content = content.replace("Wish-Logo","<img src='Img/Wish.png' width=18 height=18>")
 content = content.replace("F5Networks-Logo","<img src='Img/F5Networks.png' width=18 height=18>")
+content = content.replace("Baidu-Logo","<img src='Img/Baidu.png' width=18 height=18>")
+content = content.replace("MorganStanley-Logo","<img src='Img/MorganStanley.png' width=48 height=18>")
+
+
 content = content.replace("YouXiu-Logo","<img src='Img/YouXiuLogo.png' width=64 height=64>")
-
-
 
 content = content.replace("curve-figure","<img src='Img/curve.png'>")
 
