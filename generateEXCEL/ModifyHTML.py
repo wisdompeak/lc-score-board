800579b072fce4cedce0bb09f21e2af30ffa058d
Huifeng Guan <hguan@google.com>
2021-09-22 21:16:20 -0700

    add Coinbase

--- generateEXCEL/ModifyHTML.py
+++ generateEXCEL/ModifyHTML.py
@@ -82,6 +82,7 @@ def changeContents(content):
     content = content.replace("Instacart-Logo","<img src='Img/Instacart.png' height=18>")
     content = content.replace("Lyft-Logo","<img src='Img/Lyft.png' height=18>")
     content = content.replace("Dropbox-Logo","<img src='Img/Dropbox.png' height=18>")
+    content = content.replace("Coinbase-Logo","<img src='Img/Coinbase.svg' height=18>")
 
 
     content = content.replace("YouXiu-Logo","<img src='Img/YouXiuLogo.png' width=64 height=64>")
