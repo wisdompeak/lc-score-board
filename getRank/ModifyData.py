768b60f887d6d03f9eb46af1b0bb3cd7f2b1b621
wisdompeak <guan.huifeng@gmail.com>
2025-02-08 20:23:00 -0800

    contest 436

--- getRank/ModifyData.py
+++ getRank/ModifyData.py
@@ -2504,18 +2504,17 @@ for contest in range(118,430+1):
 # before 432
 for contest in range(133,431+1): 	
     data["Golden_Fish"][str(contest)]=[-2,0]    
-'''  
+
 
 # before 435
-for contest in range(118,434+1): 	
-    data["user5245n"][str(contest)]=[-2,0]  
 for contest in range(118,434+1): 	
     data["gauss789"][str(contest)]=[-2,0]      
 for contest in range(411,434+1): 	
     data["Tyrande"][str(contest)]=[-2,0]       
+''' 
 
-data["z2dvWZDIvN"] = data["algorithm_man"]
-data.pop("algorithm_man")
+for contest in range(118,434+1): 	
+    data["user5246n"][str(contest)]=[-2,0]  
 
 # data["tinyfish1"] = data["wxy9018"]
 # data.pop("wxy9018")
@@ -2848,7 +2847,8 @@ contests["431"] = 13029
 contests["432"] = 16711
 contests["433"] = 17758
 contests["434"] = 19472
-contests["435"] = 19347
+contests["435"] = 18874
+contests["436"] = 17142
 
 json_str = json.dumps(contests)
 
