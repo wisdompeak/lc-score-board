9edb10bfd69dcd475b8fd7cb4154ef685ce9582b
wisdompeak <guan.huifeng@gmail.com>
2025-09-06 21:48:53 -0700

    contest 466

--- getRank/ModifyData.py
+++ getRank/ModifyData.py
@@ -2564,8 +2564,12 @@ for contest in range(436,454+1):
 #     data["oaixuroab"][str(contest)]=[-2,0]  
 '''
 
-# data["Andy0315"] = data["kaijie_zhang"]
-# data.pop("kaijie_zhang")
+#before 466
+for contest in range(118,465+1): 	
+    data["susansun1110"][str(contest)]=[-2,0]  
+
+data["Razorbach"] = data["insomniacat"]
+data.pop("insomniacat")
 
 # data["ceaxyz0002"]['454'] = [1970, 3] 
 
@@ -2927,7 +2931,8 @@ contests["461"] = 20564
 contests["462"] = 20652
 contests["463"] = 13883
 contests["464"] = 21614
-contests["465"] = 20016
+contests["465"] = 19798
+contests["466"] = 20892
 
 json_str = json.dumps(contests)
 
