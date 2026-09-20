4e0bf0c7ee1343dce2b91793f4c549c9c73597b4
wisdompeak <guan.huifeng@gmail.com>
2026-09-20 01:03:46 -0700

    contest 520

--- getRank/ModifyData.py
+++ getRank/ModifyData.py
@@ -3075,7 +3075,8 @@ contests["515"] = 29541
 contests["516"] = 29633
 contests["517"] = 25000
 contests["518"] = 26478
-contests["519"] = 23645
+contests["519"] = 23339
+contests["520"] = 25544
 
 json_str = json.dumps(contests)
 
